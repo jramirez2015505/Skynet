a7fb81b08b61ed8194046e969f4990bb919d4c44
Jonathan <osic-2018030@kinal.edu.gt>
2019-09-04 08:25:24 -0600

    Creacion de base de datos

---
+++ ProyectoSkynet/src/main/java/org/skynet/database/DBSkynet.sql
@@ -0,0 +1,12 @@
+ create  database if not exists DBSkynetIN5AM;
+use DBSkynetIN5AM;
+
+create table Estudiantes(
+	codigoEstudiante int not null auto_increment,
+    carnet int not null,
+    nombres varchar(255),
+    apellidos varchar(255),
+    grado varchar(255),
+    seccion varchar(255),
+    primary key pk_codigoEstudiante(codigoEstudiante)
+);
